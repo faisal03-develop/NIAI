74cd21b371fe7e123075f8e8528a6573077c7766
Faisal Altaf <altaffaisal532@gmail.com>
2026-02-27 11:35:39 +0500

    Made and tested function to get the displacement from point a to point b in google maps using coordinates

---
+++ Day-2/Tasks/displacement.py
@@ -0,0 +1,19 @@
+import math
+
+def displacement(lat1, lon1, lat2, lon2):
+    R = 6371  # Earth radius in km
+
+    # Convert degrees to radians
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return R * c  # in kilometers
+
+
+# Example
+print(displacement(31.464042679657418, 74.44210418319426, 31.454092808708573, 74.29139241188636))  # Karachi → Lahore
