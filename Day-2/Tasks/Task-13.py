8bc63997e105b445d91f9914668ad1676a2a154a
Faisal Altaf <altaffaisal532@gmail.com>
2026-03-02 06:01:36 +0500

    Task 13-14

---
+++ Day-2/Tasks/Task-13.py
@@ -0,0 +1,9 @@
+#Nested If:
+
+i = 15
+if(i==15):
+    print('I is 10')
+    if(i>12):
+        print('I is greater the 12')
+else:
+    print('I is in else sttaement')
---
+++ Day-2/Tasks/Task-14.py
@@ -0,0 +1,10 @@
+i=10
+
+if(i==10):
+    print('I is equal to 10')
+elif(i<10):
+    print('I is less then 10')
+elif(i>10):
+    print('I is greater then 10')
+else:
+    print('I is in else OCndition')
