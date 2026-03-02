155476500bd1012001ca408630cf8ad41bf17017
Faisal Altaf <altaffaisal532@gmail.com>
2026-03-02 05:55:35 +0500

    Task 11 - Task 12

---
+++ Day-2/Tasks/Task-11.py
@@ -0,0 +1,6 @@
+#If-Else
+
+i =16
+if(i>15):
+    print('10 is less then 15')
+print('I am out of conditional Statement')
---
+++ Day-2/Tasks/Task-12.py
@@ -0,0 +1,9 @@
+i = 20
+if(i<15):
+    print('I is smaller then 15')
+    print('I am in IF Block')
+else:
+    print('i is greater then 15')
+    print('I am in the  Else Block')
+
+print('I am neither in If Nor in else')
