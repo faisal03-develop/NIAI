009300a145ac1d5865f0ac7c74f980148e5b5d1c
Faisal Altaf <altaffaisal532@gmail.com>
2026-03-02 05:50:19 +0500

    Km - Miles Converter

---
+++ Day-2/Tasks/Task-10.py
@@ -0,0 +1,9 @@
+#KM - Miles
+
+kilometers = float(input('Enter the Distance in KiloMeters: '))
+
+conv_fac = 0.621371
+
+miles = kilometers * conv_fac
+
+print('{0} Kilometers are equal to {1} Miles'.format(kilometers,miles))
